5ea1a2494e5815c411a68a46af902641685bdaf1
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-02-28 18:27:06 +1030

    created get route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,4 +3,19 @@ const path = require("path");
 let jsonDb = require("../db/db.json");
 
 module.exports = (app) => {
+    const readPromise = () =>
+    new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+        });
+    });
+
+    app.get("/api/notes", (req, res) => {
+        readPromise().then((result) => {
+            res.json(JSON.parse(result));
+        }).catch(err => console.log(err));
+    });
+
+    
 }

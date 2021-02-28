aa20e663e62a9b2d691a9b1b823a6f7cd3e45d9b
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-02-28 19:57:46 +1030

    added html route comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,25 @@
 const path = require("path");
 
 module.exports = (app)=>{
+
+    /**
+     * forward to notes page
+     */
     app.get("/notes",(request,response)=>{
         response.sendFile(path.join(__dirname,"../public/notes.html"));
     });
+
+    /**
+     * redirect to index page
+     */
 
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
+    /**
+     * loading application direct the index page
+     */
     app.get("*",(request,response)=>{
         response.sendFile(path.join(__dirname,"../public/index.html"));
     })

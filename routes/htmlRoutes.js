2ba4f6e95be486aff8b7c11fb70430870ccc378f
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-02-28 18:20:36 +1030

    created html route

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = (app)=>{
+    app.get("/notes",(request,response)=>{
+        response.sendFile(path.join(__dirname,"../public/notes.html"));
+    });
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    app.get("*",(request,response)=>{
+        response.sendFile(path.join(__dirname,"../public/index.html"));
+    })
+};

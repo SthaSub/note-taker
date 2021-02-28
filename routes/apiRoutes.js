c85725f44d64e8092c35a1731171ff590a626660
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-02-28 19:55:13 +1030

    added api route comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,9 @@ const path = require("path");
 let jsonDb = require("../db/db.json");
 
 module.exports = (app) => {
+    /**
+     * promise to provide data by reading storage file db.json
+     */
     const readPromise = () =>
     new Promise((resolve, reject) => {
         fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
@@ -11,6 +14,9 @@ module.exports = (app) => {
         });
     });
 
+    /**
+     *  promise to write new data into db.json depends upon operation as delete or post
+     */
     const writePromise = (data, operationType) =>
     new Promise((resolve, reject) => {
         let resolveResult;
@@ -30,21 +36,28 @@ module.exports = (app) => {
         });
     });
 
+    /**
+     * api of getting the all data from db.json
+     */
     app.get("/api/notes", (req, res) => {
         readPromise().then((result) => {
             res.json(JSON.parse(result));
         }).catch(err => console.log(err));
     });
 
+    /**
+     * api of adding user into db.json 
+     * */ 
     app.post("/api/notes", (req, res) => {
         if(jsonDb.length != 0){
+            // return sort of storage data in descending order
             let getSortData = jsonDb.sort((x,y)=>{
                 return parseInt(y.id) - parseInt(x.id);
             });
-            let getHighestIdAndAddOne = parseInt(getSortData[0].id) + 1;
+            let getHighestIdAndAddOne = parseInt(getSortData[0].id) + 1; // generates 'id' by adding 1 with highest 'id' of exisiting json data
             req.body.id = JSON.stringify(getHighestIdAndAddOne);
         }else{
-            req.body.id = "1";
+            req.body.id = "1"; // if json array is blank than it adds 'id' as 1
         }
         writePromise(req.body, "post")
         .then(res => console.log(res))
@@ -52,17 +65,24 @@ module.exports = (app) => {
         res.json(true);
     });
 
+    /**
+     * removes the user delete data from storage file
+     */
     app.delete("/api/notes/:id",(req,res)=>{
         readPromise().then(data=>{
             let getID = req.params.id.toString();
             let parse = JSON.parse(data);
-            let filteredData =  parse.filter(result => result.id.toString() != getID);           
+            let filteredData =  parse.filter(result => result.id.toString() != getID); // removes the data from array if ids matches           
             writePromise(filteredData,"delete")
             .then(res=>console.log(res))
             .catch(err => console.log(err));
         })
         res.json(true);
     });
+
+    /**
+     * gets the user by selection with its id
+     */
 
     app.get("/api/notes/:id", function(req,res) {
         res.json(jsonDb[req.params.id]);
--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080; // application running port
+
 
 app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+app.use(express.json()); // post the data into json format
+
+//allow express to access non js files through static location
 app.use(express.static(path.join(__dirname, "public")));
 
-
+// api routes get data, posting data and deleting data
 require("./routes/apiRoutes")(app);
+
+// html route for changing pages
 require("./routes/htmlRoutes")(app);
 
+// application request and response throught port
 app.listen(PORT,()=>{
     console.log(`connected to port: ${PORT}`);
 });
